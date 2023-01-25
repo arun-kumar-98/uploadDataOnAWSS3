39a77fef6485b9f3da995c4266a1b5e84bfc1b5a
arun-kumar-98 <arunrits15@gmail.com>
2023-01-25 17:14:40 +0530

    update

--- api/controller/uploadController.js
+++ api/controller/uploadController.js
@@ -11,7 +11,7 @@ const uploadFile = async (req, res) => {
 
       const params = {
         Bucket: process.env.BUCKET_NAME,
-        Key: process.env.bsaas_location + req.file.originalname,
+        Key: process.env.bsaas_location + /arun/ + req.file.originalname,
         Body: fileContent,
         ContentType: "image/png",
       };
@@ -60,4 +60,141 @@ const uploadFile = async (req, res) => {
   }
 };
 
-module.exports = { uploadFile };
+const getAllData = async (req, res) => {
+  try {
+    let objectData;
+
+    var getParams = {
+      Bucket: process.env.BUCKET_NAME, // your bucket name,
+      // path to the object you're looking for
+      Key: "BSaas/Test1/images.jpeg",
+    };
+
+    s3.getObject(getParams, function (err, data) {
+      // Handle any error and exit
+      if (err) return err;
+
+      console.log("=====================> ", data);
+
+      // No error happened
+      // Convert Body from a Buffer to a String
+      objectData = data.Body.toString("base64"); // Use the encoding necessary
+      console.log("#################### ", objectData);
+      res.send(objectData);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      response: error.message,
+    });
+  }
+};
+
+//find all records
+
+const findAllRecords = async (req, res) => {
+  try {
+    const getParams = {
+      Bucket: process.env.BUCKET_NAME,
+      //prefix: process.env.bsaas_location,
+    };
+    const contentList = await s3.listObjectsV2(getParams).promise();
+
+    console.log(contentList);
+    res.send(contentList);
+  } catch (error) {
+    console.log(error);
+    res.send(error.message);
+  }
+};
+
+//delete resource
+
+const deleteRecords = async (req, res) => {
+  try {
+    const getParams = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: req.body.keys,
+    };
+    s3.deleteObject(getParams, (err, data) => {
+      if (err) console.log(err);
+      console.log(data);
+      res.send(data);
+    });
+  } catch (error) {
+    console.log(error);
+    res.send(error.message);
+  }
+};
+
+//bulk  records deleted
+
+const bulkRecordsDeleted = async (req, res) => {
+  try {
+    const getParams = {
+      Bucket: process.env.BUCKET_NAME,
+      Prefix: process.env.bsaas_location,
+    };
+    const contentList = await s3.listObjectsV2(getParams).promise();
+    const arr = [];
+    contentList.Contents.forEach((element) => {
+      arr.push(element.Key);
+    });
+    console.log("array  ====", arr);
+
+    const resp = [];
+    const rec = arr.map((item) => ({
+      Key: item,
+    }));
+    console.log(rec);
+    const rec1 = await s3
+      .deleteObjects({
+        Bucket: process.env.BUCKET_NAME,
+        Delete: {
+          Objects: rec,
+          Quiet: false,
+        },
+      })
+      .promise();
+    res.send(resp);
+  } catch (error) {
+    console.log(error);
+    res.send(error.message);
+  }
+};
+
+//list of folders in bucket
+const folderListInBucket = async (req, res) => {
+  try {
+    var params = {
+      Bucket: process.env.BUCKET_NAME,
+      Delimiter: "/",
+      Prefix: "BSaas/Test1/",
+    };
+
+    s3Bucket.listObjects(params, function (err, data) {
+      if (err) {
+        return "There was an error viewing your album: " + err.message;
+      } else {
+        console.log(data.Contents, "<<<all content");
+
+        data.Contents.forEach(function (obj, index) {
+          console.log(obj.Key, "<<<file path");
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.send(error.message);
+  }
+};
+
+module.exports = {
+  uploadFile,
+  getAllData,
+  findAllRecords,
+  deleteRecords,
+  bulkRecordsDeleted,
+  bulkRecordsDeleted,
+  folderListInBucket,
+};
--- api/router/router.js
+++ api/router/router.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { uploadFile } = require("../controller/uploadController");
+const controller = require("../controller/uploadController");
 const { upload } = require("../middleware/uploadFile");
 const router = express();
 
-router.post("/upload", upload, uploadFile);
+router.post("/upload", upload, controller.uploadFile);
+router.post("/fetch", controller.getAllData);
+router.post("/findAll", controller.findAllRecords);
+router.delete("/delete", controller.deleteRecords);
+router.delete("/all", controller.bulkRecordsDeleted);
 
 module.exports = router;
